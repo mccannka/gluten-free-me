d81f2989aaa14d4975cc49f198b55ee90b76e749
mccannka <mccannkellyanne@gmail.com>
2022-05-18 16:05:16 +0000

    Navigation linkages - recipe, register & login

--- app.py
+++ app.py
@@ -4,15 +4,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#-------- Homepage ---------
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
- #-------- About --------- 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipe/recipes.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("user/register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("user/login.html")
+
 
 if __name__ == "__main__":
     app.run(
