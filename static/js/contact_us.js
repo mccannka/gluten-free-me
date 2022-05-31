55a3644c1701bf0113e4035c3d5d79af4e9a90a2
mccannka <mccannkellyanne@gmail.com>
2022-05-31 13:31:13 +0000

    Correction of EmailJS functionality

---
+++ static/js/contact_us.js
@@ -0,0 +1,23 @@
+// ----- Email Feedback form -----
+const btn = document.getElementById('button');
+
+emailjs.init('user_h27EblnJWkhrtRpVIjI8a');
+
+document.getElementById('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.value = 'Thanks - we will come back to you in 24 hours';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_f34ria5';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Contact me';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Contact me';
+        alert(JSON.stringify(err));
+      });
+  });
